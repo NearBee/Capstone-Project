407f4a41dc2a90791de4212080436aec5de7de82
Nathaniel Berube <Rexar14@gmail.com>
2023-01-31 18:39:05 -0600

    fixed url pathing

--- mealplanner/views.py
+++ mealplanner/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
     return render(
         request,
-        "mealplanner/index.html",
+        "index.html",
     )
