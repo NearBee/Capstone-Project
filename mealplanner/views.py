6fa225a247bddae282a27514be7854d700a10509
Nathaniel Berube <Rexar14@gmail.com>
2023-02-04 00:47:53 -0600

    added login and logout views

--- mealplanner/views.py
+++ mealplanner/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .forms import user_creation_form, user_login_form
 from .models import User
@@ -22,7 +24,7 @@ def register(request):
         form = user_creation_form(request.POST)
 
         # Get two passwords for confirmation
-        password2 = request.POST["passwordconfirm"]
+        password2 = form["passwordconfirm"]
         if form["password"] != password2:
             return render(request, "index.html", {"message": "Passwords must match."})
 
@@ -39,3 +41,28 @@ def register(request):
 
     else:
         return render(request, "index.html", {"message": ""})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = user_login_form(request.POST)
+
+        # Attempt to sign user in
+        email = form["email"]
+        password = form["password"]
+        user = authenticate(request, email=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "index.html", {"message": "Successfully logged in!"})
+
+    else:
+        return render(request, "index.html", {"message": ""})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
