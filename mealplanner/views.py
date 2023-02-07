6bddf2e7c8f609261bd9ff97b55ade9a6978fb6c
Nathaniel Berube <Rexar14@gmail.com>
2023-02-06 23:34:27 -0600

    Updated 'bad' path redirects

--- mealplanner/views.py
+++ mealplanner/views.py
@@ -4,33 +4,33 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .forms import user_creation_form, user_login_form
+from .forms import user_registration_form, user_login_form
 from .models import User
-
-
-# Create your views here.
 
 
 def index(request):
     return render(
         request,
         "index.html",
-        {"user_creation_form": user_creation_form, "user_login_form": user_login_form},
+        {
+            "user_registration_form": user_registration_form,
+            "user_login_form": user_login_form,
+        },
     )
 
 
 def register(request):
     if request.method == "POST":
-        form = user_creation_form(request.POST)
+        form = user_registration_form(request.POST)
 
         # Get two passwords for confirmation
         password2 = form["password_confirmation"].value()
         if form["password"].value() != password2:
-            # TODO: Should redirect to a register page
-            return redirect("index")
-
-        # POSSIBLE solution for register
-        # return render(request, "register.html")
+            return render(
+                request,
+                "register.html",
+                {"user_registration_form": user_registration_form},
+            )
 
         if form.is_valid():
             username = form["username"].value()
@@ -39,18 +39,16 @@ def register(request):
             User.objects.create_user(username, email, password)
             return redirect("index")
         else:
-            # TODO: Should redirect to a register page
-            return redirect("index")
-
-    # POSSIBLE solution for register
-    # return render(request, "register.html", {"user_creation_form": user_creation_form})
+            return render(
+                request,
+                "register.html",
+                {"user_registration_form": user_registration_form},
+            )
 
     else:
-        # TODO: Should redirect to a register page
-        return redirect("index")
-
-    # POSSIBLE solution for register
-    # return render(request, "register.html", {"user_creation_form": user_creation_form})
+        return render(
+            request, "register.html", {"user_registration_form": user_registration_form}
+        )
 
 
 def login_view(request):
@@ -67,19 +65,10 @@ def login_view(request):
             login(request, user)
             return redirect("index")
         else:
-            # Need more a clear error message for user not existing
-            # TODO: Should redirect to a full page for logging in
             message = "User does not exist"
-            # return redirect("index")
-
-            # POSSIBLE solution for failed login
             return render(request, "login.html", {"user_login_form": user_login_form})
 
     else:
-        print("Not Post")
-        # return redirect("index")
-
-        # POSSIBLE solution for failed login
         return render(request, "login.html", {"user_login_form": user_login_form})
 
 
