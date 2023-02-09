7a121df8992dff271307ebf4319d47ce401fd374
Nathaniel Berube <Rexar14@gmail.com>
2023-02-08 22:34:05 -0600

    Updated admin view

--- mealplanner/admin.py
+++ mealplanner/admin.py
@@ -1,6 +1,67 @@
 from django.contrib import admin
-from mealplanner.models import User
+from mealplanner.models import (
+    User,
+    Pantry,
+    Recipe,
+    Ingredients,
+    Planner,
+    PlannerDay,
+    Favorite,
+)
 
 # Register your models here.
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "date_joined")
+
+
+admin.site.register(User, UserAdmin)
+
+
+class PantryAdmin(admin.ModelAdmin):
+    list_display = ("id", "ingredient_name")
+
+
+admin.site.register(Pantry, PantryAdmin)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "recipe_name",
+        "recipe_instructions",
+        "nutritional_values",
+        "dietary_preference",
+    )
+
+
+admin.site.register(Recipe, RecipeAdmin)
+
+
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ("id", "pantry_item", "recipe", "quantity")
+
+
+admin.site.register(Ingredients, IngredientsAdmin)
+
+
+class PlannerAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "is_private")
+
+
+admin.site.register(Planner, PlannerAdmin)
+
+
+class PlannerDayAdmin(admin.ModelAdmin):
+    list_display = ("id", "day_of_week", "meal", "planner")
+
+
+admin.site.register(PlannerDay, PlannerDayAdmin)
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "planner")
+
+
+admin.site.register(Favorite, FavoriteAdmin)
