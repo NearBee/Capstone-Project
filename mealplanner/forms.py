48e2c7f5cac61b47f9f572920f3b85faddac5020
Nathaniel Berube <Rexar14@gmail.com>
2023-02-28 23:15:42 -0600

    Created a planner_creation form

--- mealplanner/forms.py
+++ mealplanner/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.forms import TextInput, EmailInput, PasswordInput
+from django.forms import TextInput, EmailInput, PasswordInput, Select, CheckboxInput
 
-from .models import User
+from .models import User, Planner
 
 
 class user_registration_form(forms.ModelForm):
@@ -58,4 +58,22 @@ class user_login_form(forms.ModelForm):
         labels = {
             "username": "Username",
             "password": "Password",
+        }
+
+
+class planner_creation_form(forms.ModelForm):
+    class Meta:
+        model = Planner
+        field = (
+            "days",
+            "is_private",
+        )
+        exclude = ("owner",)
+        widgets = {
+            "days": Select(attrs={"class": "form-select form-select-sm"}),
+            "is_private": CheckboxInput(attrs={"class": "form-check-input"}),
+        }
+        labels = {
+            "days": "How many days?",
+            "is_private": "Will this be private?",
         }
