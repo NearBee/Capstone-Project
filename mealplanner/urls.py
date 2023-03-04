7c4101bb6153d76ed9d95c8e0e5f002f7a6ba673
Nathaniel Berube <Rexar14@gmail.com>
2023-03-04 00:05:43 -0600

    added add_to_planner url path

--- mealplanner/urls.py
+++ mealplanner/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("recipes", views.recipes_view, name="recipes"),
     path("recipes/add_planner", views.add_planner, name="add_planner"),
+    path(
+        "recipes/add_to_planner/<int:id>", views.add_to_planner, name="add_to_planner"
+    ),
     path("favorite/<int:id>", views.favorite_recipe, name="fav_recipe"),
 ]
