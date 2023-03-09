7e8f51c1ebed828c1f6192ed9986a7fc25a8e407
Nathaniel Berube <Rexar14@gmail.com>
2023-03-09 00:00:09 -0600

    Added custom context processor

--- mealplanner/context_processors.py
+++ mealplanner/context_processors.py
@@ -1,1 +1,21 @@
 # TODO: create a context processor for information that needs to be displayed across multiple pages
+from .forms import user_registration_form, user_login_form, planner_creation_form
+from .models import Planner
+
+
+def multipage_information(request):
+
+    if not request.user.is_authenticated:
+        return {
+            "user_registration_form": user_registration_form(),
+            "user_login_form": user_login_form(),
+            "planner_form": planner_creation_form(),
+        }
+
+    return {
+        "user_registration_form": user_registration_form(),
+        "user_login_form": user_login_form(),
+        "planner_form": planner_creation_form(),
+        "number_of_plans": len(Planner.objects.filter(owner=request.user)),
+        "favorite_meals": request.user.favorite_dishes.all().count(),
+    }
