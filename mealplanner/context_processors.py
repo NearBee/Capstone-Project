61e198e735883f901d5b0d04a5a02adf05ffb38d
Nathaniel Berube <Rexar14@gmail.com>
2023-03-21 18:44:29 -0500

    Added user_edit_form to context_processors

--- mealplanner/context_processors.py
+++ mealplanner/context_processors.py
@@ -1,4 +1,9 @@
-from .forms import user_registration_form, user_login_form, planner_creation_form
+from .forms import (
+    user_registration_form,
+    user_login_form,
+    planner_creation_form,
+    user_edit_form,
+)
 from .models import Planner
 
 
@@ -14,6 +19,7 @@ def multipage_information(request):
     return {
         "user_registration_form": user_registration_form(),
         "user_login_form": user_login_form(),
+        "user_edit_form": user_edit_form(),
         "planner_form": planner_creation_form(),
         "number_of_plans": len(Planner.objects.filter(owner=request.user)),
         "favorite_meals": request.user.favorite_dishes.all().count(),
